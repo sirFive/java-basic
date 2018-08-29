2a21642aae236f6fdced60b12253447509209fc3
xie <858381526@qq.com>
2018-08-29 12:41:06 +0800

    init comit

---
+++ src/main/java/com/xie/java/basickonwledge/BasickonwledgeApplication.java
@@ -0,0 +1,12 @@
+package com.xie.java.basickonwledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasickonwledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasickonwledgeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xie/java/basickonwledge/BasickonwledgeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xie.java.basickonwledge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasickonwledgeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
